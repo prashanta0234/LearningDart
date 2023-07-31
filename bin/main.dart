eb2439a3c313d4cfe8b950971201a7863c2c71a6
prashanta0234 <prashanta0234@gmail.com>
2023-08-01 03:10:08 +0600

    add others data types

--- bin/main.dart
+++ bin/main.dart
@@ -8,6 +8,22 @@ void main() {
   int ageint = 22;
   String firstName = "Prashanta";
   String lastName = "Chakraborty";
+  // there is no float number all float will store in double
   double ageMonth = 22.9;
-  print(ageMonth);
+  Object obj = {"name": firstName + " " + lastName, "age": ageint};
+
+  // late means we will declare variable but store value later
+  late String another;
+  another = "Set value later";
+  // final and const is like js const
+
+  final nameFinal = "Pro";
+  // we can`t change final value ex:nameFinal = "abir";
+
+  const nameConst = "Const name Shanto";
+  // we can`t change const value nameConst = "abir";
+
+  dynamic ageDy = 22;
+  ageDy = ageMonth;
+  print(ageDy);
 }
