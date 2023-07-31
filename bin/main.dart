11f47bca69b88ca4f2d31f5480747c46b7f98c04
prashanta0234 <prashanta0234@gmail.com>
2023-08-01 02:58:19 +0600

    data types

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,13 @@
 void main() {
-  print("First dart code. Hello world");
+  // data types
+
+  // var is like js var we can store any types of data in var it`s not gives us type safety
+  var name = "My name is pro";
+  var age = 22;
+
+  int ageint = 22;
+  String firstName = "Prashanta";
+  String lastName = "Chakraborty";
+  double ageMonth = 22.9;
+  print(ageMonth);
 }
